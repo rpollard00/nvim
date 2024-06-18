92f3cc0e7e3a869e2b002678646f2d9177325554
Reese Pollard <rpollard@gmail.com>
2024-06-17 21:08:15 -0700

    Roslyn

--- init.lua
+++ init.lua
@@ -83,6 +83,18 @@ require('lazy').setup({
     'nvim-neotest/nvim-nio'
   },
   {
+    "iabdelkareem/csharp.nvim",
+    dependencies = {
+      "williamboman/mason.nvim", -- Required, automatically installs omnisharp
+      "mfussenegger/nvim-dap",
+      "Tastyep/structlog.nvim",  -- Optional, but highly recommended for debugging
+    },
+    config = function()
+      require("mason").setup() -- Mason setup must run before csharp
+      require("csharp").setup()
+    end
+  },
+  {
     'rcarriga/nvim-dap-ui',
     config = function()
       require("dapui").setup()
