3125340c7617f7cde116aabef697cc7f2edb1ae8
Reese <rpollard@gmail.com>
2024-07-12 12:35:25 -0700

    Win32 specific options

--- init.lua
+++ init.lua
@@ -42,8 +42,20 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
-vim.opt.shellcmdflag = '/d'
-vim.opt.shellslash = true
+
+local function set_platform_specific_options()
+  if vim.fn.has("win32") == 1 then
+    vim.opt.shellslash = true
+    vim.opt.shellcmdflag = '/d'
+    -- elseif vim.fn.has("unix") == 1 then
+    --   -- set unix/macos only options here
+    --   if vim.fn.has("mac") == 1 then
+    --     -- mac specific options here
+    --   end
+  end
+end
+
+set_platform_specific_options()
 
 -- Install package manager
 --    https://github.com/folke/lazy.nvim
